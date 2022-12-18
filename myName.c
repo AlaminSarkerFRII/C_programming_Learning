755a78d7570995f42c80ba1e6ad366f38c197333
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-12-18 20:12:38 +0600

    print your name

--- myName.c
+++ myName.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+// print your name in many times using loop
 
+#include <stdio.h>
 
 int main()
 {
 
-    int n,i;
+    int n, i;
 
     printf("Enter a Number : ");
     scanf("%d", &n);
 
-    for(i=1; i<=n ; i++)
+    for (i = 1; i <= n; i++)
     {
         printf("\nAlamin Sarker");
     }
-
 
     return 0;
 }
